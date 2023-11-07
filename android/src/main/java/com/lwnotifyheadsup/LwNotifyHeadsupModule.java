ecc037a9e4aee1854f2c21ac0a27e944a3cfd89f
codewithshinde <codewithshinde@gmail.com>
2023-11-06 20:39:45 -0600

    chore: initial commit

---
+++ android/src/main/java/com/lwnotifyheadsup/LwNotifyHeadsupModule.java
@@ -0,0 +1,32 @@
+package com.lwnotifyheadsup;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = LwNotifyHeadsupModule.NAME)
+public class LwNotifyHeadsupModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "LwNotifyHeadsup";
+
+  public LwNotifyHeadsupModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  LwNotifyHeadsupExample
+//
+
+import Foundation
